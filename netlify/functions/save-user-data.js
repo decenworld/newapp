7e7168ea57e196b4dc9b80ba4cadeea0739aad29
Steffan Jensen <6hourapp@gmail.com>
2024-09-30 07:35:49 +0200

    .

--- netlify/functions/save-user-data.js
+++ netlify/functions/save-user-data.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2/promise');
+const mariadb = require('mariadb');
 
-const pool = mysql.createPool({
+const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
